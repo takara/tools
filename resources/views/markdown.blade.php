827e4d132e70733a38ab6f81081abb302fcbe7b1
TAKA <taka2063jp@gmail.com>
2021-05-11 08:15:26 +0900

    ・markdown 追加

---
+++ resources/views/markdown.blade.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/3.0.1/github-markdown.min.css">
+</head>
+<body>
+    <div>
+        @markdown('markdowns/test')
+    </div>
+</body>
+</html>
---
+++ resources/views/markdowns/test.blade.php
@@ -0,0 +1,24 @@
+# インストール
+
+以下のコマンドを実行してください。
+
+    composer install xxxxx/xxxxx
+
+## 準備
+
+* ああして、
+* こうして、
+* こうなったら完了！
+
+***太字で注意書き***
+
+1. ここは注意
+2. ここも注意
+3. ここは絶対に注意！
+
+初めて使う場合は、`php artisan *****` を使ってください。
+
+[リンク](https://example.com)
+[すぐできる！Bladeにmarkdownが使えるディレクティブをつくる【Laravel】](https://blog.capilano-fw.com/?p=4668)
+
+![プロフィール画像](https://blog.capilano-fw.com/wp-content/uploads/2020/04/profile_piano_150x150.png)
