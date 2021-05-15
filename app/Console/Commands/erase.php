d64751072fa414a48673350adde49745d226f564
TAKA <taka2063jp@gmail.com>
2021-05-15 16:55:40 +0900

    ・tools:erase 追加

---
+++ app/Console/Commands/erase.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\BookTools;
+use Illuminate\Console\Command;
+
+class erase extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'tools:erase {paths*}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = '指定ファイルをすべてゴミ箱に入れます';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $paths           = $this->argument("paths");
+        $home		 = env("HOME");
+        $trash = "{$home}/.Trash/";
+        foreach($paths as $filename)
+        {
+            $this->info("$filename");
+            if (file_exists($filename) === false) {
+                $this->error(" ->{$filename}が見つかりません");
+                continue;
+            }
+            $cmd = "mv '{$filename}' {$trash}";
+            BookTools::exec($cmd);
+        }
+        return 0;
+    }
+}
